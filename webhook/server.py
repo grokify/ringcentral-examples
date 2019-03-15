6a69d3c5f41420114f4ba3be6d69f673f1fba862
grokify <johncwang@gmail.com>
2019-03-15 14:18:24 -0700

    streamline code

--- webhook/server.py
+++ webhook/server.py
@@ -9,6 +9,5 @@ def returnwebhook():
     if vt is not None and len(vt) > 0:
        response.set_header(vtHeader, vt)
     print(request.body.getvalue().decode('utf-8'), file=sys.stdout)
-    return
 
 run(host='localhost', port=8080)
